fd46b171cdbde218365f3bc8a1f76950d07275fb
przemskydev <przemekgithub@o2.pl>
2020-03-31 15:30:48 +0200

    add material ui

---
+++ src/components/Main.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import ButtonAppBar from './ButtonAppBar'
+
+
+export function Main() {
+  return (
+    <div>
+      <ButtonAppBar />
+      <h1>
+        Hello World
+      </h1>
+    </div>
+  )
+}
+
+// export default Main;
