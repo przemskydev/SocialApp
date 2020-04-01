43563404bf491154132301211a9066fe52d0735d
przemskydev <przemekgithub@o2.pl>
2020-04-01 15:19:00 +0200

    profile card styled

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -5,6 +5,9 @@ import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
+import logo from './img/1ok.jpg'
+import Typography from '@material-ui/core/Typography';
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -16,9 +19,15 @@ const useStyles = makeStyles((theme) => ({
     width: '70vh'
   },
   image: {
-    width: 128,
-    height: 128,
+    width: 80,
+    height: 80,
   },
+  profile: {
+    textAlign: 'center',
+  },
+  about: {
+    borderBottom: '1px solid #3F51B5',
+  }
   // img: {
   //   margin: 'auto',
   //   display: 'block',
@@ -30,12 +39,15 @@ const useStyles = makeStyles((theme) => ({
 export default function ProfileCard() {
   const classes = useStyles();
 
+
   return (
     <div className={classes.root}>
       <Paper className={classes.paper}>
         <Grid container spacing={2}>
-          <Grid item xs={12}>
-            Profile
+          <Grid item xs={12} className={classes.profile}>
+            <Typography variant="h6">
+              My profile
+            </Typography>
           </Grid>
           <Grid item xs={6}
             container
@@ -43,31 +55,40 @@ export default function ProfileCard() {
             justify="center"
             alignItems="center">
             <Grid item xs={6}>
-              <img className={classes.image} alt="logo" src='' />
+              <img className={classes.image} alt="logo" src={logo} />
             </Grid>
             <Grid item xs={6}>
-              Name
-          </Grid>
+              <Typography variant="h5">
+                Name
+            </Typography>
+            </Grid>
           </Grid>
           <Grid item xs={6}
             container
             direction="row"
-            justify="flex-end">
+            justify="flex-end"
+            alignItems="center">
             <Button variant="outlined" color="primary">
               Follow
             </Button>
           </Grid>
-          <Grid item xs={6} style={{ borderBottom: '1px solid blue' }}>
-            O MNIE
+          <Grid item xs={6} className={classes.about}>
+            <Typography variant="h6">
+              About me:
+            </Typography>
           </Grid>
           <Grid item xs={6}
             container
             direction="row"
             justify="flex-end">
-            JOINED: TIME
+            <Typography component="span" style={{ fontSize: '0.7rem' }}>
+              Joined: DATE
+            </Typography>
           </Grid>
           <Grid item xs={12}>
-            TEKST
+            <Typography component="p" style={{ padding: '1.5rem' }}>
+              TEXT
+            </Typography>
           </Grid>
           <FullWidthTabs />
         </Grid>
