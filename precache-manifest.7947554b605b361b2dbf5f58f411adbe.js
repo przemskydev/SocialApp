427ac45edf2b76b3de92a99d071e9d66c9a919ec
przemskydev <przemekgithub@o2.pl>
2020-05-13 13:27:25 +0200

    Updates

---
+++ precache-manifest.7947554b605b361b2dbf5f58f411adbe.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3114a50a7c6a0e506e20b2f07af0a6d4",
+    "url": "/socialapp/index.html"
+  },
+  {
+    "revision": "f244eaf5dc074973e3aa",
+    "url": "/socialapp/static/css/main.8231dfb9.chunk.css"
+  },
+  {
+    "revision": "8db273a0fcfc71972329",
+    "url": "/socialapp/static/js/2.17e58b69.chunk.js"
+  },
+  {
+    "revision": "673547ccae07e46414e5b78eecdf6a7b",
+    "url": "/socialapp/static/js/2.17e58b69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f244eaf5dc074973e3aa",
+    "url": "/socialapp/static/js/main.935c7999.chunk.js"
+  },
+  {
+    "revision": "634e614c9c603d8d715d",
+    "url": "/socialapp/static/js/runtime-main.b31b1967.js"
+  },
+  {
+    "revision": "ec5404312cd862223e5edc0a5a938e17",
+    "url": "/socialapp/static/media/90630.ec540431.jpg"
+  },
+  {
+    "revision": "6c4264228dc996ec738b5edef53795a5",
+    "url": "/socialapp/static/media/Bg5422.6c426422.jpg"
+  },
+  {
+    "revision": "a5c86fbe761f959e88c33c2b313020ef",
+    "url": "/socialapp/static/media/logo.a5c86fbe.svg"
+  }
+]);
